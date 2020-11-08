2c65ca63933cda765d5aaff59f6cd61706bb8828
Racer <reaganjoseph26@yahoo.com>
2020-11-08 17:24:34 -0500

    questions array set-up

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var answer = [];
+var questionsArr = [
+    {
+        questions: "What was Harry Potter's patronus?",
+        answer: true
+    },
+    {
+        question: "Who retuned in the Globet of Fire?",
+        answer: true
+    },
+    {
+        question: "What does the Imperius Curse do?",
+        answer: true
+    },
+    {
+        question: "Who killed Professor Dumbledore?",
+        answer: true
+    },
+    {
+        question: "In the end, who was revealed to be an ally of Harry Potter?",
+        answer: True
+    },
+];
+// variable to keep track of score
+var score = 0;
+
